6e7c76086bf1e7bb814ab05e349f6bee110c658f
web1n <9633157+web1n@users.noreply.github.com>
2025-01-17 19:58:38 +0800

    Add dummy EarbudsActivity

--- src/org/lineageos/xiaomi_bluetooth/BleSliceProvider.java
+++ src/org/lineageos/xiaomi_bluetooth/BleSliceProvider.java
@@ -1,17 +1,24 @@
 package org.lineageos.xiaomi_bluetooth;
 
+import android.app.PendingIntent;
 import android.bluetooth.BluetoothDevice;
 import android.content.ContentResolver;
+import android.content.Intent;
 import android.net.Uri;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.graphics.drawable.IconCompat;
 import androidx.slice.Slice;
 import androidx.slice.SliceProvider;
+import androidx.slice.builders.SliceAction;
 import androidx.slice.builders.ListBuilder;
 
+import com.android.settingslib.drawer.TileUtils;
+
 import org.lineageos.xiaomi_bluetooth.R;
+import org.lineageos.xiaomi_bluetooth.settings.EarbudsActivity;
 import org.lineageos.xiaomi_bluetooth.utils.BluetoothUtils;
 
 
@@ -35,12 +42,34 @@ public class BleSliceProvider extends SliceProvider {
 
         ListBuilder listBuilder = new ListBuilder(getContext(), sliceUri, ListBuilder.INFINITY);
 
+        ListBuilder.RowBuilder settingsActionBuilder = createSettingsActionBuilder(device);
+        if (settingsActionBuilder != null) {
+            listBuilder.addRow(settingsActionBuilder);
+        }
+
         ListBuilder.RowBuilder firmwareVersionBuilder = createFirmwareVersionBuilder(device);
         if (firmwareVersionBuilder != null) {
             listBuilder.addRow(firmwareVersionBuilder);
         }
 
         return listBuilder.build();
+    }
+
+    @Nullable
+    private ListBuilder.RowBuilder createSettingsActionBuilder(@NonNull BluetoothDevice device) {
+        if (getContext() == null) {
+            return null;
+        }
+
+        Intent intent = new Intent(getContext(), EarbudsActivity.class);
+        intent.setAction(TileUtils.IA_SETTINGS_ACTION);
+        intent.putExtra(BluetoothDevice.EXTRA_DEVICE, device);
+        PendingIntent pendingIntent = PendingIntent.getActivity(
+                getContext(), 0, intent, PendingIntent.FLAG_IMMUTABLE);
+
+        return new ListBuilder.RowBuilder()
+                .setTitle(getContext().getString(R.string.earbuds_settings))
+                .setPrimaryAction(createSliceAction(pendingIntent, R.drawable.ic_settings_24dp));
     }
 
     @Nullable
@@ -58,6 +87,17 @@ public class BleSliceProvider extends SliceProvider {
         return new ListBuilder.RowBuilder()
                 .setTitle(getContext().getString(R.string.firmware_version))
                 .setSubtitle(firmwareVersion);
+    }
+
+    @Nullable
+    private SliceAction createSliceAction(@NonNull PendingIntent pendingIntent, int iconResId) {
+        if (getContext() == null) {
+            return null;
+        }
+
+        return SliceAction.create(pendingIntent,
+                IconCompat.createWithResource(getContext(), iconResId),
+                ListBuilder.ICON_IMAGE, getContext().getString(R.string.app_name));
     }
 
     @Nullable
---
+++ src/org/lineageos/xiaomi_bluetooth/settings/EarbudsActivity.java
@@ -0,0 +1,15 @@
+package org.lineageos.xiaomi_bluetooth.settings;
+
+import android.os.Bundle;
+
+import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
+
+
+public class EarbudsActivity extends CollapsingToolbarBaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
