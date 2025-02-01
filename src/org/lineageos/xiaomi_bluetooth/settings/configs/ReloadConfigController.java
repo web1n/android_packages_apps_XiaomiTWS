f84d66f72c8d5d7665ace730e8903eb76517a3ef
Bin <9633157+web1n@users.noreply.github.com>
2025-02-01 12:36:30 +0800

    Add configuration unavailable messages and reconnect option in settings

---
+++ src/org/lineageos/xiaomi_bluetooth/settings/configs/ReloadConfigController.java
@@ -0,0 +1,51 @@
+package org.lineageos.xiaomi_bluetooth.settings.configs;
+
+import static org.lineageos.xiaomi_bluetooth.settings.EarbudsFragment.ACTION_RELOAD_CONFIG;
+
+import android.content.Context;
+import android.content.Intent;
+
+import androidx.annotation.NonNull;
+import androidx.preference.Preference;
+
+import com.android.settingslib.widget.BannerMessagePreference;
+
+import org.lineageos.xiaomi_bluetooth.EarbudsConstants;
+import org.lineageos.xiaomi_bluetooth.R;
+
+
+public class ReloadConfigController extends ConfigController {
+
+    private static final String TAG = ReloadConfigController.class.getName();
+    private static final boolean DEBUG = true;
+
+    public ReloadConfigController(@NonNull Context context, @NonNull String preferenceKey) {
+        super(context, preferenceKey);
+    }
+
+    @Override
+    public int getConfigId() {
+        return EarbudsConstants.XIAOMI_MMA_CONFIG_SN;
+    }
+
+    @Override
+    public int getExpectedConfigLength() {
+        return 20;
+    }
+
+    @Override
+    public boolean isAvailable() {
+        return !super.isAvailable();
+    }
+
+    @Override
+    public void displayPreference(@NonNull Preference preference) {
+        super.displayPreference(preference);
+
+        ((BannerMessagePreference) preference)
+                .setPositiveButtonText(R.string.reconnect_device)
+                .setPositiveButtonOnClickListener(v ->
+                        preference.getContext().sendBroadcast(new Intent(ACTION_RELOAD_CONFIG)));
+    }
+
+}
