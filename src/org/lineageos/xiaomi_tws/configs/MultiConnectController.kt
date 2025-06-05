5f743dd028e9d89d851f140d89d55af50673d3c1
Bin <9633157+web1n@users.noreply.github.com>
2025-06-05 09:00:42 +0800

    Implement mutual exclusion between multi_connect and allow_switch_device

--- src/org/lineageos/xiaomi_tws/configs/MultiConnectController.kt
+++ src/org/lineageos/xiaomi_tws/configs/MultiConnectController.kt
@@ -1,6 +1,8 @@
 package org.lineageos.xiaomi_tws.configs
 
 import android.bluetooth.BluetoothDevice
+import androidx.preference.SwitchPreference
+import androidx.preference.TwoStatePreference
 import org.lineageos.xiaomi_tws.mma.configs.MultiConnect
 
 class MultiConnectController(preferenceKey: String, device: BluetoothDevice) :
@@ -8,4 +10,10 @@ class MultiConnectController(preferenceKey: String, device: BluetoothDevice) :
 
     override val config = MultiConnect()
 
+    override fun postUpdateValue(preference: TwoStatePreference) {
+        super.postUpdateValue(preference)
+
+        preference.parent?.findPreference<SwitchPreference>("allow_switch_device")
+            ?.isEnabled = !preference.isChecked
+    }
 }
